208ba71ff9a968efa14e2cdfe861fab5ef415383
Zach Cohn <software@zachwcohn.com>
2026-02-14 20:00:26 -0600

    set up routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,20 @@
-import { createRoot } from 'react-dom/client';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./views/home";
+import Settings from "./views/settings";
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.body);
-root.render(<h2>Hello from React!</h2>);
+const App = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </HashRouter>
+  );
+};
+
+// --- Render ---
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
---
+++ src/views/settings.tsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+export default function Settings() {
+  return (
+    <><h1>Settings</h1><Link to="/">Home</Link></>
+  )
+}
